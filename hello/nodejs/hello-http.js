154964c8a2dabea9c675e9827f9c756282e9bba1
Burr Sutter <burrsutter@gmail.com>
2018-07-31 19:57:08 -0400

    getting node ready

--- hello/nodejs/hello-http.js
+++ hello/nodejs/hello-http.js
@@ -1,7 +1,9 @@
 var http = require('http');
+var cnt = 0;
+
 http.createServer(function (req, res) {
     var now = new Date();
-    res.end('Hello from Node.js! ' + now.toJSON() + ' on ' + process.env.HOSTNAME  + '\n');
+    res.end('Hello from Node.js! ' + cnt++ + ' on ' + process.env.HOSTNAME  + '\n');
     
 }).listen(8000, '0.0.0.0');
 console.log('Server running at http://:8000/');
--- hello/nodejs/readme.txt
+++ hello/nodejs/readme.txt
@@ -1,13 +1,14 @@
+
 Assumes use of CDK Minishift
 https://developers.redhat.com/products/cdk/download/
 
 minishift docker-env
 
-docker build -t burr/mynode:v1 . 
+docker build -t 9stepsawesome/mynode:v1 . 
 
 docker images | grep mynode
 
-docker run -d -p 8000:8000 burr/mynode:v1
+docker run -d -p 8000:8000 9stepsawesome/mynode:v1
 
 docker ps | grep mynode
 
