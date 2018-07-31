6829510fc016352d72798e01bec4b81c8688943f
Burr Sutter <burrsutter@gmail.com>
2018-07-30 22:15:35 -0400

    rolling update

--- hello/springboot/src/main/java/com/burrsutter/MyRESTController.java
+++ hello/springboot/src/main/java/com/burrsutter/MyRESTController.java
@@ -6,10 +6,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class MyRESTController {
    final String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
+   private int count = 0; // simple counter to see lifecycle
 
-   @RequestMapping("/hello")
+   @RequestMapping("/")
    public String sayHello() {
-       return "Hello from Spring Boot! " + new java.util.Date() + " on " + hostname + "\n";
+       return "Bonjour from Spring Boot! " + count++ + " on " + hostname + "\n";
    }
 
+   @RequestMapping("/sysresources") 
+   public String getSystemResources() {
+        long memory = Runtime.getRuntime().maxMemory();
+        int cores = Runtime.getRuntime().availableProcessors();
+        return 
+            " Memory: " + (memory / 1024 / 1024) +
+            " Cores: " + cores;
+   }
+
+   @RequestMapping("/consume") 
+   public String consumeSome() {
+       return "";
+   }
 }
