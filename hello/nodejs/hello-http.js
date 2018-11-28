4664fd04fcb4ce29025525f528d85ed7d51b8e17
Burr Sutter <burrsutter@gmail.com>
2018-11-28 16:39:02 -0500

    modifying output for single service

--- hello/nodejs/hello-http.js
+++ hello/nodejs/hello-http.js
@@ -3,7 +3,7 @@ const http = require('http');
 let cnt = 0;
 
 http.createServer((req, res) =>
-    res.end(`Hello from Node.js! ${cnt++} on ${os.hostname()}\n`)
+    res.end(`Node Hello on ${os.hostname()} ${cnt++} \n`)
 ).listen(8000);
 
 console.log(`Server running at http://localhost:8000/`);
--- hello/python/app.py
+++ hello/python/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Python Hello " + os.getenv('HOSTNAME', "unknown") + "\n"
+    return "Python Hello on " + os.getenv('HOSTNAME', "unknown") + "\n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port='8000')
