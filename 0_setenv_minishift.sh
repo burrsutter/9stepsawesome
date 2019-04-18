b9d1eefb091ef8937bee019a0a0e561d5f7ff9fb
Burr Sutter <burrsutter@gmail.com>
2019-04-18 17:44:08 -0400

    updating minishift instructions

---
+++ 0_setenv_minishift.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo 'source 0_setenv.sh'
+
+export MINISHIFT_HOME=/Users/burrsutter/minishift_1.33.0/;
+export PATH=$MINISHIFT_HOME/bin:$PATH
+# export KUBECONFIG=$MINISHIFT_HOME/.kube/config:$MINISHIFT_HOME/.kube/kubconfig2
+# export KUBECONFIG=$MINISHIFT_HOME/.kube/config
+export KUBE_EDITOR="code -w"
+
+echo 'the following after minikube as started'
+echo 'eval $(minishift --profile 9steps docker-env)'
+echo 
+echo 'Trying Kui'
+echo 'curl -sL https://tarball.kui-shell.org | tar jxf -'
+export PATH=$MINISHIFT_HOME/kui/bin:$PATH
