590a696b95cce345d66bc6b81445f555ca73d74d
Burr Sutter <burrsutter@gmail.com>
2019-07-23 22:15:43 +0530

    adding start_minikube

---
+++ start_minikube.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+minikube --profile 9steps config set memory 6144
+minikube --profile 9steps config set cpus 2 
+minikube --profile 9steps config set vm-driver virtualbox #hyperkit 
+# kubernetes version only applies to minikube
+minikube --profile 9steps config set kubernetes-version v1.13.0 
+# minishift addon enable admin-user 
+# minishift addon enable anyuid 
+minikube start --profile 9steps
