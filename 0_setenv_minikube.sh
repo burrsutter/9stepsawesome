1f33cde1b451fd3c7bb2351650c0a4fbb812e3e3
Burr Sutter <burrsutter@gmail.com>
2019-09-24 19:55:03 -0400

    removed errant /

--- 0_setenv_minikube.sh
+++ 0_setenv_minikube.sh
@@ -2,7 +2,7 @@
 
 echo 'source 0_setenv.sh'
 
-export MINIKUBE_HOME=/Users/burrsutter/minikube_1.3.1/;
+export MINIKUBE_HOME=/Users/burrsutter/minikube_1.3.1;
 export PATH=$MINIKUBE_HOME/bin:$PATH
 # export KUBECONFIG=$MINIKUBE_HOME/.kube/config:$MINIKUBE_HOME/.kube/kubconfig2
 export KUBECONFIG=$MINIKUBE_HOME/.kube/config
