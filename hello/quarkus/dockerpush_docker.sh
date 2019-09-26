69da17558b4500f112cca02b25b5be26b9dc0b1f
Burr Sutter <burrsutter@gmail.com>
2019-09-26 16:13:20 -0400

    making openshift friendly

---
+++ hello/quarkus/dockerpush_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# use docker images | grep quarkus to get the image ID for $1
+
+docker login docker.io
+
+docker tag $1 docker.io/burrsutter/quarkus-demo:1.0.0
+
+docker push docker.io/burrsutter/quarkus-demo:1.0.0
+
---
+++ hello/quarkus/dockerpush_quay.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# use docker images | grep quarkus to get the image ID for $1
+
+docker login quay.io
+
+docker tag $1 quay.io/burrsutter/quarkus-demo:1.0.0
+
+docker push quay.io/burrsutter/quarkus-demo:1.0.0
+
+echo 'quay.io marks repositories as private by default'
+echo 'to update https://screencast.com/t/uAooYnghlW'
