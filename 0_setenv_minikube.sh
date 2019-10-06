84fbeb054aa3a5780ae4a3d68aa5ca51782c2b12
Burr Sutter <burrsutter@gmail.com>
2019-10-06 17:10:19 -0400

    upgrading to minikube 1.4

--- 0_setenv_minikube.sh
+++ 0_setenv_minikube.sh
@@ -2,10 +2,10 @@
 
 echo 'source 0_setenv.sh'
 
-export MINIKUBE_HOME=/Users/burrsutter/minikube_1.3.1;
+export MINIKUBE_HOME=/Users/burrsutter/minikube;
 export PATH=$MINIKUBE_HOME/bin:$PATH
 # export KUBECONFIG=$MINIKUBE_HOME/.kube/config:$MINIKUBE_HOME/.kube/kubconfig2
-export KUBECONFIG=$MINIKUBE_HOME/.kube/config
+export KUBECONFIG=$MINIKUBE_HOME/9steps/.kube/config
 export KUBE_EDITOR="code -w"
 
 echo 'the following after minikube as started'
