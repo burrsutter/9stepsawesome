1f17c867a70fea6aa848e04e240627e748b4f70f
Burr Sutter <burrsutter@gmail.com>
2020-02-03 13:09:11 +0100

    fixing quarkus/build_push_quay.sh

--- hello/quarkus/build_push_quay.sh
+++ hello/quarkus/build_push_quay.sh
@@ -2,7 +2,7 @@
 
 IMAGE_VER=quarkus-demo:2.0.0
 
-docker build -f Dockerfile -t dev.local/burrsutter/$IMAGE_VER .
+docker build -f kubefiles/Dockerfile -t dev.local/burrsutter/$IMAGE_VER .
 docker login quay.io
 docker tag dev.local/burrsutter/$IMAGE_VER quay.io/burrsutter/$IMAGE_VER
 docker push quay.io/burrsutter/$IMAGE_VER
